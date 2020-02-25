0835669bbce32b72258e5998e45eacf525de49a9
Mustafa Siddiqui <53434234+mustafa-siddiqui@users.noreply.github.com>
2020-02-24 20:35:58 -0500

    Updated with steps to do after discussion.

--- matrixMultiplication.asm
+++ matrixMultiplication.asm
@@ -8,6 +8,8 @@
 	vectorY: .word 1, 2, 3
 	matrix: .word 5, 6, 7, 8, 9, 10
 	
+	product: .word 0:9
+	
 # instructions
 .text
 	# access to data from memory
@@ -26,6 +28,8 @@
 	add $s4, $s3, $t1			# s4 contains address of first index of VectorY >> address(vectorX[0]) + 4n
 	
 	add $s5, $s4, $t1			# s5 contains address of first index of matrix
+	
+	# find size of matrix and add to address
 	
 	# transpose Y				# don't consider this as a necessary step
 	
@@ -47,14 +51,16 @@ loop:	lw $t4, 0($s3)				# load first element of vectorX in t4
 	mflo $t6				# stores multiplication result in t6; assumes result does not exceed 32 bits
 	
 	# code works till here // maybe the next line is not doing what I intend to do 
-	sw $t7, 0($s6)				# store result of first multiply at address in s6
+	# address needs to be specified in s6; it currently stores 0x0 -> cause of error
+	sw $t6, 0($s6)				# store result of first multiply at address in s6
 	addi $s6, $s6, 4			# increment address
 	addi $s4, $s4, 4			# increment vectorY element
 	
 	bne $t3, $t8, loop			# branch for loop i (inner loop)
+	nop
 	addi $s3, $s3, 4			# increment vectorX element
 	bne $t2, $t8, loopj			# branch for loop j (outer loop)
-	
+	nop
 	
 	# matrix product multiplication
 	# printing the array
