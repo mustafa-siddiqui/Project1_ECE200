fa9102e565d301a358da2046ac00eff519a7c8b1
Mustafa Siddiqui <mustafa.eins@gmail.com>
2020-02-28 22:24:55 -0500

    Formatted & Ready to submit.

--- mergeSort.asm
+++ mergeSort.asm
@@ -3,47 +3,42 @@ length:  .word 10
 
 nums:  .word 10, 9, 8, 7, 6, 5, 4, 3, 2, 1	# we will store sorted array back here
 
-
 .text
 main:
 	lui $s0, 0x1001		# 
 	ori $s0, $s0, 0		# s0 now contains the address of length
 	addi $k0, $s0, 4	# k0 now contains the address of the first element of nums
 	lw $s0, 0($s0)		# s0 now contains length
-	nop			#
+	addi $s3, $zero, 1 	# size = 1
 	sll  $t0, $s0, 2	# we need a place on the stack for our temp array
 	sub  $k1, $sp, $t0	# k1 now contains the address of the first element of temp
-	
-# set parameters for merge sort call 
 	
 	addi $a1, $s0, -1
 	
 # mergeSort:  	# parameters:      a1 = n, #k0 = source array 
-
-	addi $s3, $zero, 1 	# size = 1
-	sub  $t3, $s3, $a1 	# initial check for outer while loop
-	bgtz $t3, final		
+		
 	addi $s1, $zero, 0 	# left = 0
 while1:	sub  $s4, $s1, $a1	# initial check for inner while loop
 	bgtz $s4, end		# s4 contains left - n
 	
-while2:	add  $s0, $s1, $s3	# these lines set mid (s0) to min(n, l + size - 1)
-	addi $s0, $s0, -1
+while2:	addi $t8, $s1, -1	# set t8 <-- l -1
+				# these lines set mid (s0) to min(n, l + size - 1)
+	add $s0, $s3, $t8
 	sub  $t5, $s0, $a1
 	addi $t5, $t5, 1
 	blez $t5, skip1
 	addi $t2, $k1, 0 	# t2 now contains pointer to L[0] nop
 	addi  $s0, $a1, -1
 	
-skip1:	sll  $s2, $s3, 1	# these lines set right (s2) to min(n, l + 2*size)
-	add  $s2, $s2, $s1
-	addi $s2, $s2, -1
+skip1:	sll  $s2, $s3, 1	# these lines set right (s2) to min(n, l + 2*size - 1)
+	add  $s2, $s2, $t8
 	sub  $t5, $s2, $a1	
 	blez $t5, skip2
 	sub $t0, $s0, $s1	# t0 = m - l
 	add  $s2, $zero, $a1
 	
 skip2:	
+	
 # at this point need s0 = m s1 = l s2 = r
 # merge:		
 	addi $t0, $t0, 1 	# t0 now contains n1
@@ -69,9 +64,9 @@ copy:
     	lw $t5, 0($t2)		# t5 contains L[i]
 	lw $t6, 0($t3)		# t6 contains R[j]
 	sll $t1, $t1, 2		# 4 * n2
+    			
+    	sub $t7, $t6, $t5	#t7 contains R[j] - L[i]
     		
-    	sub $t7, $t6, $t5	# t7 contains R[j] - L[i]
-    	
 while:	
 	bltz $t7, else		# if statement
 	sw $t5, 0($t4)		# arr[k] = L[i] #will be overwritten if branch is taken
@@ -82,7 +77,7 @@ while:
 	addi $t4, $t4, 4	# adding 4 to arr pointer
 	j while
 	sub $t7, $t6, $t5	# t7 contains R[j] - L[i]
-		
+	
 else:
 	sw $t6, 0($t4)		# arr[k] = R[j]
 	addi $t1, $t1, -4	# subtracting 4 from 4n2
@@ -101,7 +96,7 @@ whileA:
 	lw $t5, 0($t2)		# get next L[i]
 	j whileA
 	addi $t4, $t4, 4	# adding 4 to arr pointer
-	
+
 whileB:
 	sw $t6, 0($t4)		# arr[k] = R[j]
 	addi $t1, $t1, -4	# subtracting 4 from 4n2
@@ -110,10 +105,10 @@ whileB:
 	lw $t6, 0($t3)		# get next R[j]
 	j whileB
 	addi $t4, $t4, 4	# adding 4 to arr pointer
-		
+	
 donemerge:	
 	sll $t3, $s3, 1		#
-	add $s4, $t3, $s4 	# (left - n) + 2 * size
+	add $s4, $t3, $s4 	# (left - n) + 2*size
 	blez $s4, while2
 	add  $s1, $s1, $t3	# increment left
 		
@@ -136,6 +131,7 @@ L:	lw   $a0, 0($k0)	# load array[i] into a0
 	bne  $k0, $s0, L	# check for end of array
 	addi $k0, $k0, 4	# increment by 4
 	
+		
 	addi $v0, $zero, 0xa	# terminate program
 	syscall
 	
